f44d3a1d2a60071bef5f33e94d5b0c72c63235ee
Mahammadali Huseynov <93779611+MahammadAli98@users.noreply.github.com>
2023-02-09 18:26:34 +0400

    Update Mainection.js

--- src/components/Mainection.js
+++ src/components/Mainection.js
@@ -8,30 +8,46 @@ export default function Mainsection() {
     const active = JSON.parse(localStorage.getItem('active'))
     const room = localStorage.getItem('roomNum')
     const [typedefault, setTypedefault] = useState('')
+    const [userId, setUserId] = useState([])
+
+
+    const getUniqueID = () => {
+        const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+        return s4() + s4() + '-' + s4()
+    }
 
     const send = (msg) => {
-        console.log('worked ' + room)
         if (message.trim() !== '') {
-            ws.send(JSON.stringify({ type: 'message', message: msg, params: { room } }))
+            ws.send(JSON.stringify({ userId, type: 'message', message: msg, params: { room } }))
             setMessage('')
             console.log('sent messages: ' + [...messages, msg])
         }
     }
 
-    ws.onopen = () => console.log('WebSocket Client Connected' + ws.readyState)
+    ws.onopen = () => {
+        const İD = getUniqueID()
+        setUserId([...userId, İD])
+        ws.send(JSON.stringify({ userId }))
+    }
     ws.onmessage = ({ data }) => {
         const incomingData = JSON.parse(data)
-        if (incomingData.type == 'message') setMessages([...messages, incomingData.message])
+        if (incomingData.type === 'message') setMessages([...messages, incomingData.message])
         setTypedefault(incomingData.type)
     }
 
     return (
         <div className="Mainsection">
-            <input type="text" placeholder="Enter your message" name="message" value={message} onChange={(e) => { setMessage(e.target.value); if (message.trim() !== '') ws.send(JSON.stringify({ type: 'typing', message: "typing", params: { room } })) }} />
+            <input type="text" placeholder="Enter your message" name="message" value={message} 
+            onChange={(e) => {
+                 setMessage(e.target.value); 
+                 if (message.trim() !== '') ws.send(JSON.stringify({ userId, type: 'typing', message: "typing", params: { room } })) }} />
             <br></br>
-            <button disabled={(message === ' ') ? active : !active} onClick={() => send(message)}>sent message</button>
+            <button disabled={(message === ' ')
+                ? active
+                : !active}
+                onClick={() => send(message)}>sent message</button>
             <div>
-                {(typedefault == 'typing') ? <ul>typing ...</ul> : <ul> </ul>}
+                {(typedefault === 'typing') ? <ul>typing ...</ul> : null}
                 <ul>   {[...messages]?.map((item, i) => { return (<li key={i}>{item}</li>) })}    </ul>
             </div>
         </div>
