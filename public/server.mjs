3a36d1f05de292b7fd54177b3b7665074a2f5b86
Mahammadali Huseynov <93779611+MahammadAli98@users.noreply.github.com>
2023-02-09 18:27:00 +0400

    Update server.mjs

--- public/server.mjs
+++ public/server.mjs
@@ -5,34 +5,31 @@ import * as dotenv from 'dotenv'
 const app = express()
 const server = createServer(app)
 const wss = new WebSocketServer({ server })
-dotenv.config()
 const clients = {}
+let usersids
+dotenv.config()
 
-const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
-    return s4() + s4() + '-' + s4()
-}
-
-wss.on('connection', (ws) => {
-    ws.on('error', console.error)
-    let userId = getUniqueID()
-    clients[userId] = ws
-    ws.on('message', (message) => {
-        console.log(JSON.parse(message))
-        console.log(`Received message from user: ${userId} message: ${JSON.parse(message).message}`)
-        for (const client in clients) {
-            if (clients[client].readyState === WebSocket.OPEN) {
-                clients[client].send(JSON.stringify({
-                    type: JSON.parse(message).type,
-                    sender: userId,
-                    message: JSON.parse(message).message,
-                }))
+try {
+    wss.on('connection', (ws) => {
+        ws.on('error', console.error)
+        ws.on('message', (message) => {
+            clients[JSON.parse(message).userId] = ws
+            usersids = JSON.parse(message).userId
+            console.log(JSON.parse(message))
+            console.log(`Received message from user: ${JSON.parse(message).userId} message: ${JSON.parse(message).message}`)
+            for (const client in clients) {
+                if (clients[client].readyState === WebSocket.OPEN) {
+                    clients[client].send(JSON.stringify({
+                        type: JSON.parse(message).type,
+                        sender: JSON.parse(message).userId,
+                        message: JSON.parse(message).message,
+                    }))
+                }
             }
-        }
+        })
+        ws.on('close', () => { delete clients[usersids]; console.log(`User ${usersids} disconnected`) })
     })
-
-    ws.on('close', () => { console.log(`User ${userId} disconnected`); delete clients[userId] })
-})
+} catch (error) { console.error(`WebSocket error: ${error}`) }
 
 app.get('/', (req, res) => res.send('Server is up!'))
 server.listen(process.env.PORT, '0.0.0.0', () => { console.log(`Listening on port: 5000`); wss.on('error', console.error) })
